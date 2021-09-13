4f5512f65a0da89a913b5a139a969c889aa0ea8b
Philémon van Helden <philemon.vanhelden@gmx.com>
2021-09-13 11:31:44 +0200

    [ADD] Ported Minesweeper React app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './game.scss';
+import './index.scss';
+import './settings.scss';
+import Game from './game';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const btn = document.getElementById('start');
+btn.addEventListener('click', renderGame);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function renderGame() {
+    const inputs = getValidatedInputs();
+    if (!inputs.some((elem) => {return elem === null;})) {
+        ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+        ReactDOM.render(
+            <Game grid_height={inputs[0]}
+                  grid_width={inputs[1]}
+                  mines={inputs[2]}/>,
+            document.getElementById('root')
+        );
+    }
+}
+
+function getValidatedInputs() {
+    return [
+        checkRange(document.getElementById('grid-height').value, 2, 20, "Grid Height"),
+        checkRange(document.getElementById('grid-width').value, 2, 20, "Grid Width"),
+        checkRange(document.getElementById('mines').value, 1, 99, "Mines"),
+    ];
+}
+
+function checkRange(value, min, max, name) {
+    if (isNaN(value) || value < min || value > max) {
+        alert(`${name} needs to be a number between ${min} and ${max}.`);
+        return null;
+    }
+    return value;
+}
